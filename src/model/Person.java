802c3340944f1a91c3012bd52dbe6307aa4e4b6c
johanssonviktor1 <viktor@kyrkasen.se>
2025-10-29 09:55:42 +0100

    Refactor Person class to include constructor and getters

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,25 +1,47 @@
 package model;
 
-public abstract class Person implements UserType{
-	
-	@Override
-	public void getUsername() {
+public abstract class Person implements UserType {
+
+	protected String personalID;
+	protected String name;
+	protected String username;
+	protected String password;
+	protected String email;
+
+	public Person(String personalID, String name, String email, String username, String password) {
+		this.personalID = personalID;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.email = email;
 	}
-	
-	@Override
-	public void getPassword() {
+
+	public String getPersonalID() {
+		return personalID;
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	// från interfacet UserType
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
 	@Override
 	public void getPermissions() {
-	}
-	
-	public void getPersonalID() {
-		
-	}
-	
-	public void getEmail() {
-		
 	}
 
 }
