efd69ff0f9aa1be4988240407265bb1393a8ebeb
johanssonviktor1 <viktor@kyrkasen.se>
2025-10-29 09:56:02 +0100

    Add program field and constructor to Teacher class

--- src/model/Teacher.java
+++ src/model/Teacher.java
@@ -1,5 +1,15 @@
 package model;
 
-public class Teacher extends Person{
+public class Teacher extends Person {
 
+	private String program;
+	
+	public Teacher(String personalID, String name, String email, String username, String password, String program) {
+		super(personalID, name, email, username, password);
+		this.program = program;
+	}
+	
+	public String getProgram() {
+		return program;
+	}
 }
