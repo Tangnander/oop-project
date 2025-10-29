ecbb957b28b085b5fd9c0866a3e712ab4902d675
johanssonviktor1 <viktor@kyrkasen.se>
2025-10-29 09:55:30 +0100

    Update Admin.java

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -1,5 +1,9 @@
 package model;
 
-public class Admin extends Person{
+public class Admin extends Person {
 
+	public Admin(String personalID, String name, String email, String username, String password) {
+		super(personalID, name, email, username, password);
+	}
 }
+
